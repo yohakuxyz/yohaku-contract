55c63e4bf94c757021183c4b6b29df642c37cca7
shuhei tanaka <shuhei46491@gmail.com>
2024-02-28 13:27:11 +0900

    SkyBlue.t.sol init

---
+++ test/foundry/SkyBlue.t.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "../../contracts/MockNFT.sol";
+import "../../contracts/Registry.sol";
+import "../../contracts/SkyBlue.sol";
+import "../../contracts/NFTFactory.sol";
+
+contract SkyBlueTest is Test {
+    MockERC721 public mockERC721;
+    MockERC1155 public mockERC1155;
+    SkyBlue public skyblue;
+    Registry public registry;
+    NFTFactory public factory;
+
+    address public owner = makeAddr("owner");
+    address public alice = makeAddr("alice");
+    address public bob = makeAddr("bob");
+    address public charlie = makeAddr("charlie");
+
+    function setUp() public {
+        factory = deployFactory();
+        mockERC721 = deployMockERC721();
+        mockERC1155 = deployMockERC1155();
+        skyblue = new SkyBlue(owner, "", factory);
+    }
+
+    function testgetTotalPoints() public {
+        // Test for ERC721
+        mintSkyblue(alice);
+        assertEq(skyblue.getTotalPoint(0), 0);
+
+        mockERC721.safeMint(alice);
+        mockERC721.safeMint(alice);
+        mockERC721.safeMint(alice);
+        assertEq(mockERC721.balanceOf(alice), 3);
+        assertEq(skyblue.getTotalPoint(0), 3);
+
+        vm.prank(alice);
+        skyblue.approve(address(this), 0);
+        skyblue.safeTransferFrom(alice, bob, 0);
+
+        assertEq(skyblue.getTotalPoint(0), 3);
+        mockERC721.safeMint(bob);
+        mockERC721.safeMint(bob);
+
+        assertEq(skyblue.getTotalPoint(0), 5);
+
+        // Test for ERC1155
+        mockERC1155.mint(bob, 0, 1, "");
+        mockERC1155.mint(bob, 1, 1, "");
+        assertEq(mockERC1155.balanceOf(bob, 0), 1);
+        assertEq(skyblue.getTotalPoint(0), 7);
+    }
+
+    function testTransferOwnership() public {
+        mintSkyblue(alice);
+
+        vm.prank(alice);
+        skyblue.approve(address(this), 0);
+        skyblue.safeTransferFrom(alice, bob, 0);
+
+        address[] memory owners = new address[](2);
+        owners[0] = alice;
+        owners[1] = bob;
+
+        assertEq(skyblue.getOwners(0), owners);
+        assertEq(skyblue.ownerOf(0), bob);
+    }
+
+    function testSafeMint() public {
+        mintSkyblue(alice);
+        assertEq(skyblue.ownerOf(0), alice);
+    }
+
+    function mintSkyblue(address to) public {
+        vm.startPrank(owner);
+        skyblue.safeMint(to);
+        vm.stopPrank();
+    }
+
+    function deployFactory() public returns (NFTFactory) {
+        factory = new NFTFactory();
+        return factory;
+    }
+
+    function deployMockERC721() public returns (MockERC721) {
+        MockERC721 nft = factory.createERC721(1);
+        return nft;
+    }
+
+    function deployMockERC1155() public returns (MockERC1155) {
+        MockERC1155 nft = factory.createERC1155(1);
+        return nft;
+    }
+}
