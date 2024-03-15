d38e2f43c5f9075f5008b2443a0533c6649fb0a7
shuhei tanaka <shuhei46491@gmail.com>
2024-03-15 16:34:48 +0900

    added access control to resolver

--- contracts/AttesterResolver.sol
+++ contracts/AttesterResolver.sol
@@ -3,45 +3,43 @@
 pragma solidity ^0.8.20;
 
 import {SchemaResolver} from "eas-contracts/resolver/SchemaResolver.sol";
-
 import {IEAS, Attestation} from "eas-contracts/IEAS.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+error CallerNotAttester(address caller);
 
 /// @title AttesterResolver
 /// @notice A sample schema resolver that checks whether the attestation is from a specific attester.
-contract AttesterResolver is SchemaResolver {
-    address private immutable _targetAttester;
-    address[] public attesters;
+contract AttesterResolver is SchemaResolver, AccessControl {
+    address private immutable owner;
 
-    error CallerNotAttester(address caller);
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    event AttesterAdded(address indexed NewAttester);
 
     constructor(IEAS eas, address initialAttester) SchemaResolver(eas) {
-        _targetAttester = initialAttester;
-        attesters.push(initialAttester);
+        owner = initialAttester;
+        _grantRole(MINTER_ROLE, initialAttester);
     }
 
-    modifier onlyAttesters() {
-        if (!_isAttester(msg.sender)) {
-            revert CallerNotAttester(msg.sender);
+    modifier onlyAttesters(address attester) {
+        if (!hasRole(MINTER_ROLE, attester)) {
+            revert CallerNotAttester(attester);
         }
         _;
     }
 
-    // TODO: onlyAttesters
-    function addAttester(address newAttester) external /*onlyAttesters*/ {
-        attesters.push(newAttester);
-    }
-
-    function _isAttester(address attester) internal view returns (bool) {
-        for (uint256 i = 0; i < attesters.length; i++) {
-            if (attesters[i] == attester) {
-                return true;
-            }
-        }
-        return false;
+    function addAttester(address newAttester) external onlyAttesters(msg.sender) {
+        _grantRole(MINTER_ROLE, newAttester);
+        emit AttesterAdded(newAttester);
     }
 
     function onAttest(Attestation calldata attestation, uint256 /*value*/ ) internal view override returns (bool) {
-        return _isAttester(attestation.attester);
+        if (!hasRole(MINTER_ROLE, attestation.attester)) {
+            revert CallerNotAttester(attestation.attester);
+        } else {
+            return hasRole(MINTER_ROLE, attestation.attester);
+        }
     }
 
     function onRevoke(Attestation calldata, /*attestation*/ uint256 /*value*/ ) internal pure override returns (bool) {
