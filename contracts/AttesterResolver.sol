189cd253cd71d291cea758a51cc8b987c89b706c
shuhei tanaka <shuhei46491@gmail.com>
2024-03-15 15:28:52 +0900

    attest when minted

--- contracts/AttesterResolver.sol
+++ contracts/AttesterResolver.sol
@@ -10,13 +10,38 @@ import {IEAS, Attestation} from "eas-contracts/IEAS.sol";
 /// @notice A sample schema resolver that checks whether the attestation is from a specific attester.
 contract AttesterResolver is SchemaResolver {
     address private immutable _targetAttester;
+    address[] public attesters;
 
-    constructor(IEAS eas, address targetAttester) SchemaResolver(eas) {
-        _targetAttester = targetAttester;
+    error CallerNotAttester(address caller);
+
+    constructor(IEAS eas, address initialAttester) SchemaResolver(eas) {
+        _targetAttester = initialAttester;
+        attesters.push(initialAttester);
+    }
+
+    modifier onlyAttesters() {
+        if (!_isAttester(msg.sender)) {
+            revert CallerNotAttester(msg.sender);
+        }
+        _;
+    }
+
+    // TODO: onlyAttesters
+    function addAttester(address newAttester) external /*onlyAttesters*/ {
+        attesters.push(newAttester);
+    }
+
+    function _isAttester(address attester) internal view returns (bool) {
+        for (uint256 i = 0; i < attesters.length; i++) {
+            if (attesters[i] == attester) {
+                return true;
+            }
+        }
+        return false;
     }
 
     function onAttest(Attestation calldata attestation, uint256 /*value*/ ) internal view override returns (bool) {
-        return attestation.attester == _targetAttester;
+        return _isAttester(attestation.attester);
     }
 
     function onRevoke(Attestation calldata, /*attestation*/ uint256 /*value*/ ) internal pure override returns (bool) {
