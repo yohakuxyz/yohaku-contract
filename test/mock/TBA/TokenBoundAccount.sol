cc1d25d19393f51c1f8c7e8e76da030d4c620a84
shuhei tanaka <shuhei46491@gmail.com>
2025-03-26 20:05:25 +0900

    chore: fmt

--- test/mock/TBA/TokenBoundAccount.sol
+++ test/mock/TBA/TokenBoundAccount.sol
@@ -10,18 +10,12 @@ import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import {IERC6551Account} from "erc6551/interfaces/IERC6551Account.sol";
-import {IERC6551Executable} from "erc6551/interfaces/IERC6551Executable.sol";
+import { IERC6551Account } from "erc6551/interfaces/IERC6551Account.sol";
+import { IERC6551Executable } from "erc6551/interfaces/IERC6551Executable.sol";
 
 error InvalidChainId();
 
-contract TokenBoundAccount is
-    Ownable,
-    IERC6551Account,
-    IERC6551Executable,
-    IERC721Receiver,
-    IERC1155Receiver
-{
+contract TokenBoundAccount is Ownable, IERC6551Account, IERC6551Executable, IERC721Receiver, IERC1155Receiver {
     constructor() Ownable(msg.sender) {
         owners.push(msg.sender);
     }
@@ -31,21 +25,26 @@ contract TokenBoundAccount is
     /// inherit IERC6551Account
     uint256 public state;
 
-    receive() external payable {}
+    receive() external payable { }
 
     function execute(
         address to,
         uint256 value,
         bytes calldata data,
         uint8 operation
-    ) external payable virtual returns (bytes memory result) {
+    )
+        external
+        payable
+        virtual
+        returns (bytes memory result)
+    {
         require(_isValidSigner(msg.sender), "Invalid signer");
         require(operation == 0, "Only call operations are supported");
 
         ++state;
 
         bool success;
-        (success, result) = to.call{value: value}(data);
+        (success, result) = to.call{ value: value }(data);
 
         if (!success) {
             assembly {
@@ -54,10 +53,7 @@ contract TokenBoundAccount is
         }
     }
 
-    function isValidSigner(
-        address signer,
-        bytes calldata
-    ) external view virtual returns (bytes4) {
+    function isValidSigner(address signer, bytes calldata) external view virtual returns (bytes4) {
         if (_isValidSigner(signer)) {
             return IERC6551Account.isValidSigner.selector;
         }
@@ -65,15 +61,8 @@ contract TokenBoundAccount is
         return bytes4(0);
     }
 
-    function isValidSignature(
-        bytes32 hash,
-        bytes memory signature
-    ) external view virtual returns (bytes4 magicValue) {
-        bool isValid = SignatureChecker.isValidSignatureNow(
-            owner(),
-            hash,
-            signature
-        );
+    function isValidSignature(bytes32 hash, bytes memory signature) external view virtual returns (bytes4 magicValue) {
+        bool isValid = SignatureChecker.isValidSignatureNow(owner(), hash, signature);
 
         if (isValid) {
             return IERC1271.isValidSignature.selector;
@@ -82,21 +71,12 @@ contract TokenBoundAccount is
         return bytes4(0);
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) external pure virtual returns (bool) {
-        return
-            interfaceId == type(IERC165).interfaceId ||
-            interfaceId == type(IERC6551Account).interfaceId ||
-            interfaceId == type(IERC6551Executable).interfaceId;
+    function supportsInterface(bytes4 interfaceId) external pure virtual returns (bool) {
+        return interfaceId == type(IERC165).interfaceId || interfaceId == type(IERC6551Account).interfaceId
+            || interfaceId == type(IERC6551Executable).interfaceId;
     }
 
-    function token()
-        public
-        view
-        virtual
-        returns (uint256 chainId, address tokenContract, uint256 tokenId)
-    {
+    function token() public view virtual returns (uint256 chainId, address tokenContract, uint256 tokenId) {
         bytes memory footer = new bytes(0x60);
 
         assembly {
@@ -115,23 +95,12 @@ contract TokenBoundAccount is
     }
 
     /// @inheritdoc IERC721Receiver
-    function onERC721Received(
-        address,
-        address,
-        uint256,
-        bytes calldata
-    ) external pure returns (bytes4) {
+    function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
     }
 
     /// @inheritdoc IERC1155Receiver
-    function onERC1155Received(
-        address,
-        address,
-        uint256,
-        uint256,
-        bytes memory
-    ) public virtual returns (bytes4) {
+    function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
         return this.onERC1155Received.selector;
     }
 
@@ -142,13 +111,15 @@ contract TokenBoundAccount is
         uint256[] memory,
         uint256[] memory,
         bytes memory
-    ) public virtual returns (bytes4) {
+    )
+        public
+        virtual
+        returns (bytes4)
+    {
         return this.onERC1155BatchReceived.selector;
     }
 
-    function _isValidSigner(
-        address signer
-    ) internal view virtual returns (bool) {
+    function _isValidSigner(address signer) internal view virtual returns (bool) {
         return signer == owner();
     }
 }
--- test/mock/YohakuV2.sol
+++ test/mock/YohakuV2.sol
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {Yohaku} from "../../contracts/Yohaku.sol";
+import { Yohaku } from "../../contracts/Yohaku.sol";
 
 /// @custom:oz-upgrades-from Yohaku
 contract YohakuV2 is Yohaku {
-    function initializeV2(
-        address owner,
-        string memory description,
-        string memory imageURL
-    ) public reinitializer(2) {
+    function initializeV2(address owner, string memory description, string memory imageURL) public reinitializer(2) {
         defaultImageUrl = imageURL;
         defaultDescription = description;
         _grantRole(DEFAULT_ADMIN_ROLE, owner);
@@ -19,9 +15,7 @@ contract YohakuV2 is Yohaku {
         return "v2.0.0";
     }
 
-    function revokeMinter(
-        address minter
-    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function revokeMinter(address minter) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _revokeRole(MINTER_ROLE, minter);
     }
 }
